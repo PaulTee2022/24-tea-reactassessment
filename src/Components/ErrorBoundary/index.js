76939cc8bd5e8fc0e247be46530f322b2837fc1f
PalitTea2525 <palitlojanakosin@gmail.com>
2022-11-15 12:01:14 +0700

    first commit

---
+++ src/Components/ErrorBoundary/index.js
@@ -0,0 +1,19 @@
+class ErrorBoundary extends React.Component {
+    constructor(props) {
+        super(props) 
+        this.state = {error : null, errorInfo : null};  
+    } 
+     componentDidCatch(error, errorInfo) {
+        this.setState({error : error, errorInfo : errorInfo}); 
+    } 
+    render() {
+        if (this.state.errorInfo) {
+            return 
+            <div>
+            <h1>เกิดข้อผิดพลาด</h1>
+            <details>{this.state.error && this.state.error.toString()}<br />{this.state.errorInfo.componentStack}</details>
+            </div>
+        } 
+        return this.props.children 
+    }
+}
---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './TrackList.css';
+
+class TrackList extends React.Component {
+    render() {
+        return (
+            <div className="TrackList">
+            <hr />
+            <h3>Tried out f/ start for 4 times already</h3>
+            <p>(I reached No.55 & ran out of time, can't debug No.34, 44, 52)</p>
+            <hr />
+            <p>Might try again when got better skill, Thanks.</p>
+            </div>
+        )
+    }
+}
+
+export default TrackList;
